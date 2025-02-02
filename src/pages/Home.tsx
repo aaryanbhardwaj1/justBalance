35b95c9ec4872b8381aec7134c3cff4a48fa5beb
bhavya091213 <bhavya091213@gmail.com>
2025-02-02 12:00:27 -0500

    get started

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,20 @@
-import React from 'react'
+import React from "react";
 
-import justBalanceLogo from '../assets/justBalanced2.png'
-import meal1 from '../assets/meal1.jpg'
-import meal2 from '../assets/meal2.jpg'
-import meal3 from '../assets/meal3.jpeg'
-import meal4 from '../assets/meal4.jpg'
-import meal5 from '../assets/meal5.jpg'
-import meal6 from '../assets/meal6.jpg'
+import justBalanceLogo from "../assets/justBalanced2.png";
+import meal1 from "../assets/meal1.jpg";
+import meal2 from "../assets/meal2.jpg";
+import meal3 from "../assets/meal3.jpeg";
+import meal4 from "../assets/meal4.jpg";
+import meal5 from "../assets/meal5.jpg";
+import meal6 from "../assets/meal6.jpg";
+import { Link, useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate(); // Redirect after successful login
   return (
     <>
-      
-      <div className="split left background">          
-      <div className="centered">
+      <div className="split left background">
+        <div className="centered">
           {/* <img src={justBalanceLogo} className="logo" alt="JustBalanceLogo" />
           <h1>AI-Powered Nutrition</h1>
           <h2>Turn Ingredients into Delicious Meals!</h2> */}
@@ -21,36 +22,41 @@ const Home = () => {
       </div>
 
       <div className="split right">
-          <div className="centered mediumbox">
-            <span><img src={justBalanceLogo} className="logo" alt="JustBalanceLogo" /></span>
-            <div>
-            <h2 className='typewriter'>Smart. Simple. Nutritious.</h2>
-            </div>
-            <div>
-            <h3>Powered by AI, We help you create healthy, <br/> balanced diets tailored to your lifestyle.</h3>
-            </div>
-            <div className="carousel-container">
-
+        <div className="centered mediumbox">
+          <span>
+            <img src={justBalanceLogo} className="logo" alt="JustBalanceLogo" />
+          </span>
+          <div>
+            <h2 className="typewriter">Smart. Simple. Nutritious.</h2>
+          </div>
+          <div>
+            <h3>
+              Powered by AI, We help you create healthy, <br /> balanced diets
+              tailored to your lifestyle.
+            </h3>
+          </div>
+          <div className="carousel-container">
             <div className="imagecarousel">
-
-              <img src={meal1} className='meal'/>
-              <img src={meal2} className='meal' />
-              <img src={meal3} className='meal' />
-              <img src={meal4} className='meal' />
-              <img src={meal5} className='meal' />
-              <img src={meal6} className='meal' />  
-
-              </div>
-              </div>
-
-            <button>Get started</button>
+              <img src={meal1} className="meal" />
+              <img src={meal2} className="meal" />
+              <img src={meal3} className="meal" />
+              <img src={meal4} className="meal" />
+              <img src={meal5} className="meal" />
+              <img src={meal6} className="meal" />
+            </div>
           </div>
 
+          <button
+            onClick={() => {
+              navigate("/login");
+            }}
+          >
+            Get started
+          </button>
+        </div>
       </div>
-
-      
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
